44a93f79495ca55ea5908523a29b6ca507c1d016
1855532 <aditi.s.akella@gmail.com>
2021-02-21 20:43:46 -0800

    l

--- views.py
+++ views.py
@@ -1,13 +1,6 @@
 """Views in MVC has responsibility for establishing routes and redering HTML"""
-from flask import render_template, request, redirect, url_for
+from flask import render_template
 from __init__ import app
-import requests
-
-"""Main navigation Section"""
-
-
-# This section of code is driven by data, review data descriptions for understanding
-
 
 @app.route('/')
 def index():
