d2c54e761760771f4ec295eed46899e04ad5b3b8
1855532 <aditi.s.akella@gmail.com>
2021-02-22 14:43:19 -0800

    database working

--- models/__init__.py
+++ models/__init__.py
@@ -1,12 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
-from control import app
+from app import app
 
 dbURI = 'sqlite:///models/myDB.db'
 
 """ database setup to support db examples """
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = dbURI
-app.config['SECRET_KEY'] = 'SECRET_KEY'
 db = SQLAlchemy(app)
 
 
@@ -27,15 +26,3 @@ class Emails(db.Model):
 class PhoneNumbers(db.Model):
     UserID = db.Column(db.Integer, primary_key=True)
     phone_number = db.Column(db.String(255), unique=True, nullable=False)
-
-
-# declare authorised user account model
-class AuthUser(db.Model):
-    __tablename__ = 'authuser'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False, unique=False)
-    email = db.Column(db.String(40), unique=True, nullable=False)
-    password = db.Column(db.String(200), primary_key=False, unique=False, nullable=False)
-    website = db.Column(db.String(60), index=False, unique=False, nullable=True)
-    created_on = db.Column(db.DateTime, index=False, unique=False, nullable=True)
-    last_login = db.Column(db.DateTime, index=False, unique=False, nullable=True)
